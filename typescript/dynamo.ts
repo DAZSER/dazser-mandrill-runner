c1dc05d8090cb05fd516cf1bc4d375a20c65c777
Kyle McNally <kyle@kmcnally.net>
2019-02-12 14:35:30 -0500

    Add project files

---
+++ typescript/dynamo.ts
@@ -0,0 +1,55 @@
+import DynamoDB from "aws-sdk/clients/dynamodb";
+import SQS from "aws-sdk/clients/sqs";
+
+// Configure AWS
+const ddb = new DynamoDB({apiVersion: "2012-08-10"});
+
+export default async function(messageId: string, receiptHandle: string) {
+
+  // verify that we haven't seen this payload.
+  const ddbExists: DynamoDB.GetItemInput = {
+    AttributesToGet: [
+      "id",
+    ],
+    ConsistentRead: true,
+    Key: {
+      "id": {
+        S: messageId,
+      },
+    },
+    TableName: process.env.DYNAMODB_TABLE as string,
+  };
+  const exists = await ddb.getItem(ddbExists).promise();
+
+  if (exists.Item) {
+    // if exists then return skip
+    // console.log("Item Exists!", messageId);
+    // we should also remove the item from sqs
+    const sqs = new SQS({apiVersion: "2012-11-05"});
+    const sqsDelete: SQS.DeleteMessageRequest = {
+      QueueUrl: process.env.SQS_QUEUE_URL as string,
+      ReceiptHandle: receiptHandle,
+    };
+    sqs.deleteMessage(sqsDelete);
+    return true;
+  } else {
+    // if it doesn't exist , enter it, then continue
+    const ddbPut: DynamoDB.PutItemInput = {
+      Item: {
+        "expires": {
+          S: (Date.now() + 3.6e+6).toString(),
+        },
+        "id": {
+          S: messageId,
+        },
+      },
+      TableName: process.env.DYNAMODB_TABLE as string,
+    };
+
+    const putItem = await ddb.putItem(ddbPut).promise();
+
+    console.log(putItem);
+
+    return false;
+  }
+}
---
+++ typescript/handler.ts
@@ -0,0 +1,42 @@
+import * as request from "request-promise-native";
+import checkMessage from "./dynamo";
+
+export const sendPayloadToMandrill = async (event: AWSLambda.SQSEvent, context: AWSLambda.Context) => {
+
+  // This function will send the payload to mandrill
+  // console.log(JSON.stringify(event));
+  const options = {
+    body: {},
+    headers: {
+      "Content-Type": "application/json",
+    },
+    json: true,
+    method: "POST",
+    uri: "https://mandrillapp.com/api/1.0/messages/send-template.json",
+  };
+
+  // Loop through the event.Records
+  for (const record of event.Records) {
+
+    if (await checkMessage(record.messageId, record.receiptHandle)) {
+      console.log("Duplicate!", record);
+      continue;
+    }
+
+    // Get the body of the record. It's JSON
+    const json = JSON.parse(record.body);
+
+    // For each payload in the json body
+    for (const payload of json) {
+      // Take the payload and send it to Mandrill
+      options.body = payload;
+
+      // Now that we have the json set up, call mandrill
+      const result = await request.post(options);
+
+      console.log(result);
+
+    }
+  }
+  return { message: "Payloads sent successfully!" };
+};
